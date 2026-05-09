fe03310194579b2e705f873a4ee4fd38c950f4fc
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-09 12:41:44 +0700

    feat(map): add map page skeleton and basic state management

---
+++ lib/map_page.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _ctrl = Completer();
+  Marker? _pickedMarker;
+  String? _pickedAddress;
+  String? _currentAddress;
+  CameraPosition? _initialcamera;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pilih Alamat"),
+      ),
+      body: const Center(
+        child: Text("Map will be here"),
+      ),
+    );
+  }
+}
